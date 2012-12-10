ecb4b804add4567e78786b39d205bfb8f76e0585
Fabricio Zuardi <fabricio@fabricio.org>
2012-12-10 16:09:47 -0200

    updates to the home page template and generator, to use a fullscreen background image

--- bin/build_website.js
+++ bin/build_website.js
@@ -1,26 +1,25 @@
 //libraries
-var fs = require('fs'),
+var fs = require('fs-extra'),
     path = require('path'),
-    mkdirp = require('mkdirp'),
     mustache = require('mustache');
 
 //local setup
 var BUILD_FOLDER = path.join(__dirname, '../www'),
     TEMPLATE_FOLDER = path.join(__dirname, '../templates'),
     DATA_FOLDER = path.join(__dirname, '../data'),
-    PAGE_NAME = 'onibushacker';
+    PAGE_NAME = 'onibushacker',
+    STATIC_FOLDERS = ['css', 'js', 'img'];
 
 //read the contents of the main json
-var indexJson = fs.readFileSync(path.join(DATA_FOLDER,PAGE_NAME,'index.json'), 'utf-8'),
-    coverJson = fs.readFileSync(path.join(DATA_FOLDER,PAGE_NAME,'cover.json'), 'utf-8'),
-    albumListJson = fs.readFileSync(path.join(DATA_FOLDER,PAGE_NAME,'albums','index.json'), 'utf-8'),
-    indexTemplate = fs.readFileSync(path.join(TEMPLATE_FOLDER,'index.html'), 'utf-8'),
-    indexData = JSON.parse(indexJson),
-    coverData = JSON.parse(coverJson),
-    albumListData = JSON.parse(albumListJson);
+var indexTemplate = fs.readFileSync(path.join(TEMPLATE_FOLDER,'index.html'), 'utf-8'),
+    indexData = fs.readJSONFileSync(path.join(DATA_FOLDER,PAGE_NAME,'index.json')),
+    coverData = fs.readJSONFileSync(path.join(DATA_FOLDER,PAGE_NAME,'cover.json')),
+    albumListData = fs.readJSONFileSync(path.join(DATA_FOLDER,PAGE_NAME,'albums','index.json'));
 
 //replace the cover image source with the bigger picture
 indexData.cover.source = coverData.images[0].source;
+indexData.cover.width = coverData.images[0].width;
+indexData.cover.height = coverData.images[0].height;
 indexData.albums = albumListData.albums.data;
 
 //save the generated index.html
@@ -32,4 +31,19 @@ fs.writeFile(path.join(BUILD_FOLDER,'index.html'),
   }
 );
 
-console.log(mustache.render(indexTemplate, indexData));
+//copy static folders
+STATIC_FOLDERS.forEach(
+  function copyStaticFolder(folderName, index){
+    fs.copy(path.join(TEMPLATE_FOLDER,folderName), path.join(BUILD_FOLDER,folderName),
+      function folderCopied(err){
+        if (err) { console.error(err); }
+        else {
+          console.log("Static folder "+ folderName +" copied.");
+        }
+      }
+    );
+  }//copyStaticFolder
+);
+
+
+// console.log(mustache.render(indexTemplate, indexData));
